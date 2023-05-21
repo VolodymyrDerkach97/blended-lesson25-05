50b0f8f832e1d5d6e99fb5583351b271464f1431
Volodymyr Derkach <bobikderkac@gmail.com>
2023-05-21 11:13:09 +0300

    refactor hook

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,15 +1,33 @@
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { Text } from 'components';
 import { DeleteButton, TodoWrapper } from './Todo.styled';
+import { useState } from 'react';
 
-export const Todo = ({ text, counter, onClick, id }) => {
+export const Todo = ({ text, counter, onClick, id, onEdit }) => {
+  const [newText, setNewText] = useState(text);
+  const [isEdit, setIsEdit] = useState(false);
+
   return (
     <>
       <TodoWrapper>
         <Text textAlign="center" marginBottom="20px">
           TODO #{counter}
         </Text>
-        <Text>{text}</Text>
+        {isEdit ? (
+          <input
+            type="text"
+            autoFocus
+            value={newText}
+            onBlur={() => {
+              setIsEdit(false);
+              onEdit({ id, text: newText });
+            }}
+            onChange={e => setNewText(e.target.value)}
+          />
+        ) : (
+          <Text onClick={() => setIsEdit(true)}>{text}</Text>
+        )}
+
         <DeleteButton type="button" onClick={() => onClick(id)}>
           <RiDeleteBinLine size={24} />
         </DeleteButton>
